e35ef2380b7db8c0203a62402fc0b4b1b0d221e2
manindrarana <ranamanindra9@gmail.com>
2026-02-24 13:53:24 +0000

    updated financial data ingestion to use Yahoo Finance API and remove Alpha Vantage integration, and made logic to retrieve data hourly from yfinance and left to fix for other data sources

---
+++ src/ingestion/yahoo_finance.py
@@ -0,0 +1,63 @@
+import os
+import yaml
+import pandas as pd
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import yfinance as yf
+
+class YahooFinanceClient:
+    def __init__(self):
+        with open("configs/settings.yml", "r") as f:
+            self.config = yaml.safe_load(f)
+            
+        self.raw_path = self.config["paths"]["raw_data"]
+        os.makedirs(self.raw_path, exist_ok=True)
+
+    def fetch_data(self, ticker: str):
+        """
+        Fetches historical data from Yahoo Finance and saves as Parquet.
+        """
+        print(f"Fetching data for {ticker} using yfinance...")
+        
+        config_start_date = self.config["ingestion"]["settings"]["start_date"]
+        interval = self.config["providers"]["yfinance"]["interval"] 
+        
+        start_date = config_start_date
+        if interval == "1h":
+            limit_date = (datetime.now() - timedelta(days=700)).strftime("%Y-%m-%d")
+            
+            if config_start_date < limit_date:
+                print(f"Adjusting start_date for 1h data: {config_start_date} -> {limit_date}")
+                start_date = limit_date
+        
+        try:
+            df = yf.download(ticker, start=start_date, interval=interval, progress=False)
+            
+            if df.empty:
+                print(f"No data found for {ticker}")
+                return None
+
+            if isinstance(df.columns, pd.MultiIndex):
+                df.columns = df.columns.get_level_values(0)
+
+            df.reset_index(inplace=True)
+
+            df.columns = [c.lower().replace(" ", "_") for c in df.columns]
+            
+            timestamp = datetime.now().strftime("%Y-%m-%d")
+            filename = f"{ticker}_{timestamp}.parquet"
+            file_path = os.path.join(self.raw_path, filename)
+            
+            df.to_parquet(file_path, index=False)
+            
+            print(f"Success! Saved {len(df)} rows to {file_path}")
+            return file_path
+
+        except Exception as e:
+            print(f"Failed to fetch {ticker}: {e}")
+            return None
+
+if __name__ == "__main__":
+    client = YahooFinanceClient()
+    client.fetch_data("AAPL")
+
