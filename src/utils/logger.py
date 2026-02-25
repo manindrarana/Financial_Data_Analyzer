f90bb04c6a2b6949e55f2f77d4f6f7b7437ad619
manindrarana <ranamanindra9@gmail.com>
2026-02-25 21:10:27 +0000

    updated  logger utility and also added __init__.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -13,3 +13,22 @@ def get_logger(name: str) -> logging.Logger:
     Returns:
         logging.Logger: A logger you can use to print messages
     """
+    logger = logging.getLogger(name)
+
+    level_str = os.getenv("LOG_LEVEL", "INFO").upper()
+    level = getattr(logging, level_str, logging.INFO)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(level)
+
+        formatter = logging.Formatter(
+            '%(asctime)s [%(levelname)s] [%(name)s]: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(console_handler)
+
+    return logger
