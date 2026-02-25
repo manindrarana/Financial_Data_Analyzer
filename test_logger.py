41f028f98e036f07470532c338b955d52fabc5ce
manindrarana <ranamanindra9@gmail.com>
2026-02-25 21:26:00 +0000

    cleanup logger tests and finalize issue #2
    Co-authored-by: Manindra Rana <a46249@alunos.ipb.pt>

--- test_logger.py
+++
@@ -1,8 +0,0 @@
-from src.utils import get_logger
-
-logger = get_logger("TestModule")
-
-logger.debug("This is a debug message (You might not see this if LOG_LEVEL=INFO)")
-logger.info("This is an info message (Standard output)")
-logger.warning("This is a warning!")
-logger.error("This is an error message!")
