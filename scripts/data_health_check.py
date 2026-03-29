7b61aa5e1a44caa85465e23751d0b6f2ea836ba9
manindrarana <ranamanindra9@gmail.com>
2026-03-29 22:19:35 +0100

    added database  connection in healthcheckpy

--- scripts/data_health_check.py
+++ scripts/data_health_check.py
@@ -5,3 +5,17 @@ import os
 from src.utils import get_logger
 
 logger = get_logger("HealthCheck")
+
+
+def get_db_connection():
+    """Loads config and returns the DuckDB connection."""
+    with open("configs/settings.yml", "r") as f:
+        config = yaml.safe_load(f)
+    
+    db_path = config["paths"]["database"]
+    
+    if not os.path.exists(db_path):
+        logger.error(f"Database not found at {db_path}!")
+        return None
+        
+    return duckdb.connect(db_path, read_only=True)
