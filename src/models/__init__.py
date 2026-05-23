b1862e331524ac6754729e88cbfb6219f280c95f
manindrarana <ranamanindra9@gmail.com>
2026-05-23 16:33:56 +0100

    registered ModelPredictor in models __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .logics import GoldLayerProcessor
 from .technical_indicators import TechnicalIndicatorProcessor
 from .feature_analyzer import FeatureAnalyzer
+from .predict import ModelPredictor
